cbd2e21b06acbdcb825c9933d29908bf26b779ef
jrmarino <draco@marino.st>
2017-11-07 15:44:57 -0600

    Add linux support for hints file

--- libpkg/elfhints.c
+++ libpkg/elfhints.c
@@ -305,6 +305,11 @@ shlib_list_from_elf_hints(const char *hintsfile)
 #if defined __FreeBSD__ || defined __DragonFly__
 	read_elf_hints(hintsfile, 1);
 #endif
+#if defined __linux__
+	add_dir ("fake-elf-hints-1", "/lib/x86_64-linux-gnu", 1);
+	add_dir ("fake-elf-hints-2", "/usr/lib/x86_64-linux-gnu", 1);
+	add_dir ("fake-elf-hints-3", "/lib64", 1);
+#endif
 
 	return (scan_dirs_for_shlibs(&shlibs, ndirs, dirs, true));
 }
